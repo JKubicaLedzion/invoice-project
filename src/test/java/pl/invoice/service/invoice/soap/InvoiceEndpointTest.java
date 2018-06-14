dfb2d2edbbbbf4a7276e8060e44705d9dc1d114d
jk <j.kubica.ledzion@gmail.com>
2018-06-14 19:56:44 +0200

    XML test requests and responses moved to XML files

--- src/test/java/pl/invoice/service/invoice/soap/InvoiceEndpointTest.java
+++ src/test/java/pl/invoice/service/invoice/soap/InvoiceEndpointTest.java
@@ -3,30 +3,31 @@ package pl.invoice.service.invoice.soap;
 import static org.springframework.ws.test.server.RequestCreators.withPayload;
 import static org.springframework.ws.test.server.ResponseMatchers.noFault;
 import static org.springframework.ws.test.server.ResponseMatchers.payload;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.ADD_FIRST_INVOICE_REQUEST;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.ADD_INVOICE_RESPONSE;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.ADD_INVOICE_WRONG_REQUEST;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.ADD_SECOND_INVOICE_REQUEST;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.ADD_UPDATE_INVOICE_WRONG_DATA_RESPONSE;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.DELETE_INVOICE_REQUEST;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.GET_INVOICES_REQUEST;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.GET_INVOICES_RESPONSE;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.GET_INVOICES_WITHIN_DATE_RANGE_REQUEST;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.GET_INVOICES_WITHIN_DATE_RANGE_RESPONSE;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.GET_INVOICE_BY_ID_REQUEST;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.GET_INVOICE_BY_ID_REQUEST_INVOICE_NOT_FOUND;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.GET_INVOICE_BY_ID_RESPONSE;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.GET_INVOICE_BY_ID_RESPONSE_AFTER_UPDATE;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.GET_INVOICE_BY_ID_RESPONSE_INVOICE_NOT_FOUND;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.UPDATE_INVOICE_REQUEST;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.UPDATE_INVOICE_RESPONSE;
-import static pl.invoice.service.invoice.soap.XmlRequestAndResponse.UPDATE_INVOICE_WRONG_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.ADD_FIRST_INVOICE_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.ADD_INVOICE_RESPONSE;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.ADD_INVOICE_WRONG_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.ADD_SECOND_INVOICE_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.ADD_UPDATE_INVOICE_WRONG_DATA_RESPONSE;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.BASE_PATH_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.BASE_PATH_RESPONSE;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.DELETE_INVOICE_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.GET_INVOICES_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.GET_INVOICES_RESPONSE;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.GET_INVOICES_WITHIN_DATE_RANGE_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.GET_INVOICES_WITHIN_DATE_RANGE_RESPONSE;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.GET_INVOICE_BY_ID_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.GET_INVOICE_BY_ID_REQUEST_INVOICE_NOT_FOUND;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.GET_INVOICE_BY_ID_RESPONSE;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.GET_INVOICE_BY_ID_RESPONSE_AFTER_UPDATE;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.GET_INVOICE_BY_ID_RESPONSE_INVOICE_NOT_FOUND;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.UPDATE_INVOICE_REQUEST;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.UPDATE_INVOICE_RESPONSE;
+import static pl.invoice.service.invoice.soap.XmlEnvelopePath.UPDATE_INVOICE_WRONG_REQUEST;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -39,12 +40,15 @@ import org.springframework.ws.test.server.MockWebServiceClient;
 import org.springframework.xml.transform.StringSource;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 import javax.xml.transform.Source;
 
 @TestPropertySource(value = "classpath:application-soap.properties")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class InvoiceEndpointTest {
 
@@ -62,16 +66,16 @@ public class InvoiceEndpointTest {
   @Test
   public void getInvoicesShouldReturnInvoiceList() {
     // given:
-    Source requestAddInvoice = new StringSource(ADD_FIRST_INVOICE_REQUEST);
-    Source requestAddSecondInvoice = new StringSource(ADD_SECOND_INVOICE_REQUEST);
-    Source requestGetInvoice = new StringSource(GET_INVOICES_REQUEST);
-    Source responseGetInvoice = new StringSource(GET_INVOICES_RESPONSE);
-    mockClient
-        .sendRequest(withPayload(requestAddInvoice))
-        .andExpect(noFault());
-    mockClient
-        .sendRequest(withPayload(requestAddSecondInvoice))
-        .andExpect(noFault());
+    Source requestAddInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_FIRST_INVOICE_REQUEST));
+    Source requestAddSecondInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_SECOND_INVOICE_REQUEST));
+    Source requestGetInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + GET_INVOICES_REQUEST));
+    Source responseGetInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_RESPONSE + GET_INVOICES_RESPONSE));
+    mockClient.sendRequest(withPayload(requestAddInvoice)).andExpect(noFault());
+    mockClient.sendRequest(withPayload(requestAddSecondInvoice)).andExpect(noFault());
     // when:
     mockClient
         .sendRequest(withPayload(requestGetInvoice))
@@ -82,12 +86,13 @@ public class InvoiceEndpointTest {
   @Test
   public void getInvoiceByIdShouldReturnResponseWithInvoice() {
     // given:
-    Source requestAddInvoice = new StringSource(ADD_FIRST_INVOICE_REQUEST);
-    Source requestGetInvoice = new StringSource(GET_INVOICE_BY_ID_REQUEST);
-    Source responseGetInvoice = new StringSource(GET_INVOICE_BY_ID_RESPONSE);
-    mockClient
-        .sendRequest(withPayload(requestAddInvoice))
-        .andExpect(noFault());
+    Source requestAddInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_FIRST_INVOICE_REQUEST));
+    Source requestGetInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + GET_INVOICE_BY_ID_REQUEST));
+    Source responseGetInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_RESPONSE + GET_INVOICE_BY_ID_RESPONSE));
+    mockClient.sendRequest(withPayload(requestAddInvoice)).andExpect(noFault());
     // when:
     mockClient
         .sendRequest(withPayload(requestGetInvoice))
@@ -98,16 +103,18 @@ public class InvoiceEndpointTest {
   @Test
   public void getInvoicesWithinDateRangeShouldReturnResponseWithInvoiceList() {
     // given:
-    Source requestAddFirstInvoice = new StringSource(ADD_FIRST_INVOICE_REQUEST);
-    Source requestAddSecondInvoice = new StringSource(ADD_SECOND_INVOICE_REQUEST);
-    Source requestGetInvoices = new StringSource(GET_INVOICES_WITHIN_DATE_RANGE_REQUEST);
-    Source responseGetInvoices = new StringSource(GET_INVOICES_WITHIN_DATE_RANGE_RESPONSE);
-    mockClient
-        .sendRequest(withPayload(requestAddFirstInvoice))
-        .andExpect(noFault());
-    mockClient
-        .sendRequest(withPayload(requestAddSecondInvoice))
-        .andExpect(noFault());
+    Source requestAddFirstInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_FIRST_INVOICE_REQUEST));
+    Source requestAddSecondInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_SECOND_INVOICE_REQUEST));
+    Source requestGetInvoices =
+        new StringSource(
+            getSoapEnvelope(BASE_PATH_REQUEST + GET_INVOICES_WITHIN_DATE_RANGE_REQUEST));
+    Source responseGetInvoices =
+        new StringSource(
+            getSoapEnvelope(BASE_PATH_RESPONSE + GET_INVOICES_WITHIN_DATE_RANGE_RESPONSE));
+    mockClient.sendRequest(withPayload(requestAddFirstInvoice)).andExpect(noFault());
+    mockClient.sendRequest(withPayload(requestAddSecondInvoice)).andExpect(noFault());
     // when:
     mockClient
         .sendRequest(withPayload(requestGetInvoices))
@@ -118,8 +125,9 @@ public class InvoiceEndpointTest {
   @Test
   public void addInvoiceShouldAddInvoice() {
     // given:
-    Source request = new StringSource(ADD_FIRST_INVOICE_REQUEST);
-    Source response = new StringSource(ADD_INVOICE_RESPONSE);
+    Source request =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_FIRST_INVOICE_REQUEST));
+    Source response = new StringSource(getSoapEnvelope(BASE_PATH_RESPONSE + ADD_INVOICE_RESPONSE));
     // when:
     mockClient
         .sendRequest(withPayload(request))
@@ -130,8 +138,11 @@ public class InvoiceEndpointTest {
   @Test
   public void addInvoiceShouldReturnResponseWithFault() {
     // given:
-    Source request = new StringSource(ADD_INVOICE_WRONG_REQUEST);
-    Source response = new StringSource(ADD_UPDATE_INVOICE_WRONG_DATA_RESPONSE);
+    Source request =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_INVOICE_WRONG_REQUEST));
+    Source response =
+        new StringSource(
+            getSoapEnvelope(BASE_PATH_RESPONSE + ADD_UPDATE_INVOICE_WRONG_DATA_RESPONSE));
     // when:
     mockClient
         .sendRequest(withPayload(request))
@@ -142,12 +153,14 @@ public class InvoiceEndpointTest {
   @Test
   public void updateInvoiceShouldReturnResponseWithFault() throws IOException {
     // given:
-    Source requestAddInvoice = new StringSource(ADD_FIRST_INVOICE_REQUEST);
-    Source requestUpdateInvoice = new StringSource(UPDATE_INVOICE_WRONG_REQUEST);
-    Source responseUpdateInvoice = new StringSource(ADD_UPDATE_INVOICE_WRONG_DATA_RESPONSE);
-    mockClient
-        .sendRequest(withPayload(requestAddInvoice))
-        .andExpect(noFault());
+    Source requestAddInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_FIRST_INVOICE_REQUEST));
+    Source requestUpdateInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + UPDATE_INVOICE_WRONG_REQUEST));
+    Source responseUpdateInvoice =
+        new StringSource(
+            getSoapEnvelope(BASE_PATH_RESPONSE + ADD_UPDATE_INVOICE_WRONG_DATA_RESPONSE));
+    mockClient.sendRequest(withPayload(requestAddInvoice)).andExpect(noFault());
     // when:
     mockClient
         .sendRequest(withPayload(requestUpdateInvoice))
@@ -158,14 +171,18 @@ public class InvoiceEndpointTest {
   @Test
   public void updateInvoiceShouldUpdateExistingInvoice() {
     // given:
-    Source requestAddInvoice = new StringSource(ADD_FIRST_INVOICE_REQUEST);
-    Source requestUpdateInvoice = new StringSource(UPDATE_INVOICE_REQUEST);
-    Source responseUpdateInvoice = new StringSource(UPDATE_INVOICE_RESPONSE);
-    Source requestGetInvoice = new StringSource(GET_INVOICE_BY_ID_REQUEST);
-    Source responseGetInvoice = new StringSource(GET_INVOICE_BY_ID_RESPONSE_AFTER_UPDATE);
-    mockClient
-        .sendRequest(withPayload(requestAddInvoice))
-        .andExpect(noFault());
+    Source requestAddInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_FIRST_INVOICE_REQUEST));
+    Source requestUpdateInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + UPDATE_INVOICE_REQUEST));
+    Source responseUpdateInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_RESPONSE + UPDATE_INVOICE_RESPONSE));
+    Source requestGetInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + GET_INVOICE_BY_ID_REQUEST));
+    Source responseGetInvoice =
+        new StringSource(
+            getSoapEnvelope(BASE_PATH_RESPONSE + GET_INVOICE_BY_ID_RESPONSE_AFTER_UPDATE));
+    mockClient.sendRequest(withPayload(requestAddInvoice)).andExpect(noFault());
     // when:
     mockClient
         .sendRequest(withPayload(requestUpdateInvoice))
@@ -181,8 +198,12 @@ public class InvoiceEndpointTest {
   @Test
   public void getInvoiceByIdShouldReturnResponseWithFault() {
     // given:
-    Source requestGetInvoice = new StringSource(GET_INVOICE_BY_ID_REQUEST_INVOICE_NOT_FOUND);
-    Source responseGetInvoice = new StringSource(GET_INVOICE_BY_ID_RESPONSE_INVOICE_NOT_FOUND);
+    Source requestGetInvoice =
+        new StringSource(
+            getSoapEnvelope(BASE_PATH_REQUEST + GET_INVOICE_BY_ID_REQUEST_INVOICE_NOT_FOUND));
+    Source responseGetInvoice =
+        new StringSource(
+            getSoapEnvelope(BASE_PATH_RESPONSE + GET_INVOICE_BY_ID_RESPONSE_INVOICE_NOT_FOUND));
     // when:
     mockClient
         .sendRequest(withPayload(requestGetInvoice))
@@ -193,15 +214,20 @@ public class InvoiceEndpointTest {
   @Test
   public void deleteInvoiceShouldDeleteInvoice() {
     // given:
-    Source requestAddInvoice = new StringSource(ADD_FIRST_INVOICE_REQUEST);
-    Source requestDeleteInvoice = new StringSource(DELETE_INVOICE_REQUEST);
-    Source requestGetInvoice = new StringSource(GET_INVOICE_BY_ID_REQUEST_INVOICE_NOT_FOUND);
-    Source responseGetInvoiceBeforeDelete = new StringSource(GET_INVOICE_BY_ID_RESPONSE);
-    Source responseGetInvoiceAfterDelete = new StringSource(GET_INVOICE_BY_ID_RESPONSE_INVOICE_NOT_FOUND);
+    Source requestAddInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + ADD_FIRST_INVOICE_REQUEST));
+    Source requestDeleteInvoice =
+        new StringSource(getSoapEnvelope(BASE_PATH_REQUEST + DELETE_INVOICE_REQUEST));
+    Source requestGetInvoice =
+        new StringSource(
+            getSoapEnvelope(BASE_PATH_REQUEST + GET_INVOICE_BY_ID_REQUEST_INVOICE_NOT_FOUND));
+    Source responseGetInvoiceBeforeDelete =
+        new StringSource(getSoapEnvelope(BASE_PATH_RESPONSE + GET_INVOICE_BY_ID_RESPONSE));
+    Source responseGetInvoiceAfterDelete =
+        new StringSource(
+            getSoapEnvelope(BASE_PATH_RESPONSE + GET_INVOICE_BY_ID_RESPONSE_INVOICE_NOT_FOUND));
 
-    mockClient
-        .sendRequest(withPayload(requestAddInvoice))
-        .andExpect(noFault());
+    mockClient.sendRequest(withPayload(requestAddInvoice)).andExpect(noFault());
     mockClient
         .sendRequest(withPayload(requestGetInvoice))
         .andExpect(payload(responseGetInvoiceBeforeDelete));
@@ -213,5 +239,15 @@ public class InvoiceEndpointTest {
     mockClient
         .sendRequest(withPayload(requestGetInvoice))
         .andExpect(payload(responseGetInvoiceAfterDelete));
+  }
+
+  private String getSoapEnvelope(String filePath) {
+    StringBuilder stringBuilder = new StringBuilder();
+    try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
+      stream.forEach(s -> stringBuilder.append(s));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return stringBuilder.toString();
   }
 }
---
+++ src/test/java/pl/invoice/service/invoice/soap/XmlEnvelopePath.java
@@ -0,0 +1,44 @@
+package pl.invoice.service.invoice.soap;
+
+public class XmlEnvelopePath {
+
+  public static final String BASE_PATH_REQUEST = "src/test/resources/xmlRequest/";
+
+  public static final String BASE_PATH_RESPONSE = "src/test/resources/xmlResponse/";
+
+  public static final String GET_INVOICE_BY_ID_REQUEST_INVOICE_NOT_FOUND = "getInvoiceByIdRequestInvoiceNotFound.xml";
+
+  public static final String GET_INVOICE_BY_ID_REQUEST = "getInvoiceByIdRequest.xml";
+
+  public static final String GET_INVOICES_WITHIN_DATE_RANGE_REQUEST = "getInvoicesWithinDateRangeRequest.xml";
+
+  public static final String GET_INVOICE_BY_ID_RESPONSE_INVOICE_NOT_FOUND = "getInvoiceByIdResposneInvoiceNotFound.xml";
+
+  public static final String ADD_UPDATE_INVOICE_WRONG_DATA_RESPONSE = "addUpdateInvoiceWrongDataResponse.xml";
+
+  public static final String GET_INVOICE_BY_ID_RESPONSE = "getInvoiceByIdResponse.xml";
+
+  public static final String GET_INVOICE_BY_ID_RESPONSE_AFTER_UPDATE = "getInvoiceByIdResponseAfterUpdate.xml";
+
+  public static final String ADD_FIRST_INVOICE_REQUEST = "addFirstInvoiceRequest.xml";
+
+  public static final String ADD_INVOICE_WRONG_REQUEST = "addWrongInvoiceRequest.xml";
+
+  public static final String ADD_SECOND_INVOICE_REQUEST = "addSecondInvoiceRequest.xml";
+
+  public static final String UPDATE_INVOICE_REQUEST = "updateInvoiceRequest.xml";
+
+  public static final String UPDATE_INVOICE_WRONG_REQUEST = "updateInvoiceWrongRequest.xml";
+
+  public static final String ADD_INVOICE_RESPONSE = "addInvoiceResponse.xml";
+
+  public static final String UPDATE_INVOICE_RESPONSE = "updateInvoiceResponse.xml";
+
+  public static final String GET_INVOICES_REQUEST = "getInvoicesRequest.xml";
+
+  public static final String GET_INVOICES_WITHIN_DATE_RANGE_RESPONSE = "getInvoicesWithinDateRangeResponse.xml";
+
+  public static final String GET_INVOICES_RESPONSE = "getInvoicesResponse.xml";
+
+  public static final String DELETE_INVOICE_REQUEST = "deleteInvoiceRequest.xml";
+}
